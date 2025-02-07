196a46449b6c458dc92b73a3cf1b66db1e6911dd
GAP-Pathum <pathumpasindu41@gmail.com>
2025-02-07 23:55:35 +0530

    refactor: remove password hashing logic from Student model

--- lms-backend/models/Student.js
+++ lms-backend/models/Student.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const studentSchema = new mongoose.Schema({
   studentNumber: {
@@ -32,11 +31,5 @@ const studentSchema = new mongoose.Schema({
   }
 });
 
-// Hash password before saving
-// studentSchema.pre('save', async function(next) {
-//   if (!this.isModified('password')) return next();
-//   this.password = await bcrypt.hash(this.password, 10);
-//   next();
-// });
 
 module.exports = mongoose.model('Student', studentSchema);
